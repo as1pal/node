ee839cbe066b37a857d614d3a47d29eaae7041fb
as1pal <skn97@kakao.com>
2021-05-25 13:19:31 +0900

    관리자 설정

--- routes/member.js
+++ routes/member.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
 const member = require("../models/member"); //member model  
+const { joinValidator } = require('../middlewares/join_validator'); //회원가입 유효성검사
+
 router.route("/join")
     /** 회원 가입 양식 */
     .get((req, res, next) => {
         res.render("member/form");
     })
     /** 회원 가입 처리 */
-    .post((req, res, next) => {
-        console.log(req.body);
-        req.send("");
+    .post(joinValidator, async (req, res, next) => {
+        try {
+            const result = await member.join(req.memId, req.memPw);
+        } catch (err) {
+            console.error(err);
+            next(err);
+        }
     });
 
 module.exports = router;
