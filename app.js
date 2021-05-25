c018a6c17985acb3a4234b84d4d51032dcd2646e
as1pal <skn97@kakao.com>
2021-05-25 11:20:23 +0900

    git ignore .env 추가

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(methodOverride("_method"));
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cookieParser(process.env.COOKIE_SECRET)); //쿠키 설정
 
 // 없는 페이지 처리 미들웨어(라우터)
 app.use((req, res, next) => {
