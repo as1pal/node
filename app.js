5520c057e2418b42ba46b17fe793b49869d4b118
as1pal <skn97@kakao.com>
2021-05-25 11:17:44 +0900

    app.js 수정

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require("path");
+const path = require('path');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
@@ -12,30 +12,31 @@ dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set("view engine", "html");
-
 nunjucks.configure("views", {
-    express: app,
-    watch: true,
+	express : app,
+	watch : true,
 });
 
 app.use(morgan('dev'));
+app.use(methodOverride("_method"));
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//없는 페이지 처리 미들웨어 (라우터)
+
+// 없는 페이지 처리 미들웨어(라우터)
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url}은 없는 페이지 입니다.`);
-    error.status = 404;
-    next(error); //에러 처리 미들웨어로 전달
+	const error = new Error(`${req.method} ${req.url}은 없는 페이지 입니다.`);
+	error.status = 404;
+	next(error); // 에러 처리 미들웨어로 전달 
 });
 
-
-
-//에러 처리 미들웨어 (라우터)
+// 에러 처리 미들웨어(라우터)
 app.use((err, req, res, next) => {
-    res.locals.error = err;
-
-    res.status(err.status || 500).render('error');
+	res.locals.error = err;
+	res.status(err.status || 500).render('error');
 });
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port') "번 포트에서 대기중");
+	console.log(app.get('port'), '번 포트에서 대기중');
 });
