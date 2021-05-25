2d8d3cdea1ffcad42edbc6ece865c6d2fe07cbd4
as1pal <skn97@kakao.com>
2021-05-25 12:04:17 +0900

    데이터베이스 설정 회원가입양식

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const dotenv = require('dotenv');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const { sequelize } = require("./models");
+
+/**라우터 */
+const memberRouter = require('./routes/member');
 
 dotenv.config();
 
@@ -16,6 +20,14 @@ nunjucks.configure("views", {
 	express : app,
 	watch : true,
 });
+/** db 연결 */
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log("데이터 베이스 연결 성공");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.use(morgan('dev'));
 app.use(methodOverride("_method"));
@@ -33,6 +45,8 @@ app.use(session({
     name : 'wjsession',
 }));
 
+/**라우터 등록 */
+app.use("/member", memberRouter);
 
 // 없는 페이지 처리 미들웨어(라우터)
 app.use((req, res, next) => {
