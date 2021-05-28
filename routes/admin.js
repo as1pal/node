7e8480e796480df298f92fca8650eb93dd342f0c
as1pal <skn97@kakao.com>
2021-05-28 09:23:02 +0900

    table set

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
-/**
-* 이력서 관리 페이지
-*/
+const resume = require("../models/resume");
+const { alert, reload } = require("../lib/common");
 const express = require('express');
 const router = express.Router();
 
+/**
+* 이력서 관리 페이지
+*/
 router.get("/", (req, res, next) => {
 	const params = {
 		addClass : 'admin_page',
@@ -11,8 +13,14 @@ router.get("/", (req, res, next) => {
 	return res.render("admin/main", params);
 });
 
-router.post("/profile", (req, res, next) => {
-	console.log(req.body);
-	res.send("");
+/** 이력서 저장 처리 */
+router.post("/profile", async (req, res, next) => {
+	const result = await resume.update(req.body);
+	if (!result)  {
+		return alert("이력서 저장에 실패하였습니다.", res);
+	}
+	
+	return reload(res, "parent");
 });
+
 module.exports = router;
