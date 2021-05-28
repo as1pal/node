085cc393c5e40f5bd2702ebf43ed971c94fcd9a6
as1pal <skn97@kakao.com>
2021-05-28 11:29:26 +0900

    ..

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(morgan('dev'));
 app.use(methodOverride("_method"));
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/", express.static(path.join(__dirname, 'public')));
 app.use(cookieParser(process.env.COOKIE_SECRET)); // 쿠기 설정 
 app.use(session({
 	resave: false,
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,90 +1,156 @@
 /** 
-* 저장된 이력서 호출 
-*
-*/
-function getResume()
-{
+ * 저장된 이력서 호출 
+ *
+ */
+function getResume() {
 	$.ajax({
-		url : "/admin/profile",
-		type : "get",
-		dataType : "html",
-		success : function (res) {
-			console.log(res);
+		url: "/admin/profile",
+		type: "get",
+		dataType: "json",
+		success: function (res) {
+			/* basicinfo */
+			if (res.basicinfo) {
+				for (key in res.basicinfo) {
+					$target = $("input[name='" + key + "']");
+					if ($target.length > 0) {
+						switch ($target.attr("type")) {
+							case "text":
+							case "email":
+								$target.val(res.basicinfo[key]);
+								break;
+							case "checkbox":
+								if (res.basicinfo[key] instanceof Array) { // 복수 checkbox
+									// 취업우대, 병역 노출 
+									if (res.basicinfo[key].length > 0) {
+										$("section.benefit").removeClass("dn");
+										$("#selection_items10").prop("checked", true);
+									}
+
+									$.each($target, function () {
+										const v = $(this).val();
+										let checked = false;
+										if (res.basicinfo[key].indexOf(v) != -1) {
+											checked = true;
+
+											if (v == '장애') {
+												$(".additional_select, .additional_select .handicap").removeClass("dn");
+											} else if (v == '병역') {
+												$(".additional_select, .additional_select .military").removeClass("dn");
+											}
+										}
+
+										$(this).prop("checked", checked);
+									});
+								} else { // 단일 checkbox 
+									$target.prop("checked", res.basicinfo[key]);
+								}
+								break;
+						}
+					}
+				}
+			} // endif
+			/* basicinfo */
+
+			/** 나머지 테이블 처리 */
+			for (table in res) {
+				if (table == 'basicinfo') continue;
+
+				let type = "";
+				switch (table) {
+					case "award":
+						type = "수상";
+						break;
+					case "education":
+						type = "교육";
+						break;
+					case "intern":
+						type = "인턴";
+						break;
+					case "introduction":
+						type = "자기소개";
+						break;
+					case "jobhistory":
+						type = "경력";
+						break;
+				}
+
+			}
+
+			/** 나머지 테이블 처리 */
+
 		},
-		error : function (err) {
+		error: function (err) {
 			console.error(err);
 		}
 	});
 }
 
 /**
-* 템플릿 양식 추가 처리 
-*
-*/
-function addForm(type, target)
-{
+ * 템플릿 양식 추가 처리 
+ *
+ */
+function addForm(type, target) {
 	let template = "";
 	switch (type) {
-		case "학력" : 
+		case "학력":
 			template = "school";
 			break;
-		case "경력" : 
+		case "경력":
 			template = "job_history";
 			break;
-		case "인턴" : 
+		case "인턴":
 			template = "intern";
 			break;
-		case "교육" : 
+		case "교육":
 			template = "education";
 			break;
-		case "자격증" : 
-			template = "license"; 
+		case "자격증":
+			template = "license";
 			break;
-		case "수상" : 
+		case "수상":
 			template = "award";
 			break;
-		case "해외경험" : 
-			template = "overseas"; 
+		case "해외경험":
+			template = "overseas";
 			break;
-		case "어학" : 
+		case "어학":
 			template = "language";
 			break;
-		case "자기소개" : 
+		case "자기소개":
 			template = "introduction";
 			break;
-		case "포트폴리오" :
+		case "포트폴리오":
 			template = "portfolio";
 			break;
 	}
-		
+
 	if (template) {
 		let html = $("#template_" + template).html();
-					
+
 		if (target.length > 0) {
 			const no = new Date().getTime();
 			html = html.replace(/<%=no%>/g, no);
 		}
-			
+
 		target.append(html);
-	} 
+	}
 }
 
 /**
-* 선택한 메뉴에 따라 이력서 양식 항목 노출,미노출 처리
-*
-*/
-function updateSelectedMenu()
-{
+ * 선택한 메뉴에 따라 이력서 양식 항목 노출,미노출 처리
+ *
+ */
+function updateSelectedMenu() {
 	$list = $(".floating_box input[type='checkbox']");
-	$.each($list, function() {
+	$.each($list, function () {
 		const target = $(this).data("target");
 		$target = $("section." + target);
 		if ($(this).prop("checked")) {
 			$target.removeClass("dn");
-			
+
 			/** 현재 영역에 추가된 입력 양식이 없으면 추가 */
 			const cnt = $target.find(".rows").length;
-			$form = $target.find(".add_form"); 
+			$form = $target.find(".add_form");
 			if (cnt == 0 && $form.length > 0) { // 양식 추가 항목 중에서 추가된 양식이 없는 경우 자동 추가 
 				const type = $form.data("type");
 				$formHtml = $target.find(".form_html");
@@ -97,11 +163,10 @@ function updateSelectedMenu()
 }
 
 /**
-* 스크롤시 오른쪽 floating 메뉴 고정 
-*
-*/
-function updateNavFixed()
-{
+ * 스크롤시 오른쪽 floating 메뉴 고정 
+ *
+ */
+function updateNavFixed() {
 	const offset = $(".container .nav").offset();
 	const ypos = offset.top + 100;
 	const st = $(window).scrollTop();
@@ -114,16 +179,15 @@ function updateNavFixed()
 }
 
 /** 
-* 취업우대,병역 항목 선택시 
-* 장애, 병역 선택에 따른 항목 노출 
-*
-*/
-function updateBenefit()
-{
+ * 취업우대,병역 항목 선택시 
+ * 장애, 병역 선택에 따른 항목 노출 
+ *
+ */
+function updateBenefit() {
 	$list = $(".benefit input[type='checkbox']:checked");
 	let isAdditionalSelect = false;
 	$(".additional_select, .additional_select dl").removeClass("dn").addClass("dn");
-	$.each($list, function() {
+	$.each($list, function () {
 		const benefit = $(this).val();
 		if (benefit == '장애') {
 			isAdditionalSelect = true;
@@ -133,19 +197,18 @@ function updateBenefit()
 			$(".additional_select .military").removeClass("dn");
 		}
 	});
-	
+
 	if (isAdditionalSelect) {
 		$(".additional_select").removeClass("dn");
 	}
 }
 
 /**
-* 프로필 사진 업로드 처리
-*
-* @param Boolean isSuccess - true 업로드 성공, false - 실패
-*/
-function uploadCallback(isSuccess) 
-{
+ * 프로필 사진 업로드 처리
+ *
+ * @param Boolean isSuccess - true 업로드 성공, false - 실패
+ */
+function uploadCallback(isSuccess) {
 	/**
 		성공 
 				1. 프로필 사진 -> 사진 추가 영역에 붙여 넣고 
@@ -160,70 +223,70 @@ function uploadCallback(isSuccess)
 	} else { // 실패 
 		alert("이미지 업로드 실패");
 	}
-	
+
 	layer.close();
 }
 
-$(function() {
+$(function () {
 	/** 저장된 이력서 호출 */
 	getResume();
-	
+
 	/** 파일 업로드 처리 */
-	$("body").on("change", ".upload_box input[type='file']", function() {
+	$("body").on("change", ".upload_box input[type='file']", function () {
 		frmUpload.submit();
 	});
-	
+
 	/** 주소 검색 */
-	$(".search_address").click(function() {
-		 new daum.Postcode({
-			oncomplete: function(data) {
+	$(".search_address").click(function () {
+		new daum.Postcode({
+			oncomplete: function (data) {
 				$("input[name='address']").val(data.address);
 			}
 		}).open();
 	});
-	
+
 	/** 양식 추가 처리 */
-	$(".add_form").click(function() {
+	$(".add_form").click(function () {
 		const type = $(this).data("type");
 		$target = $(this).closest(".form_inner").find(".form_html");
 		addForm(type, $target);
 	});
-	
+
 	/** 양식 제거 처리 */
-	$("body").on("click", ".form_html .remove", function() {
+	$("body").on("click", ".form_html .remove", function () {
 		$(this).closest(".rows").remove();
 	});
-	
-	
+
+
 	/** textarea 확대 축소 처리 */
-	$("body").on("focus", ".form_html textarea", function() {
+	$("body").on("focus", ".form_html textarea", function () {
 		if (!$(this).hasClass("intro")) {
 			$(this).removeClass("h200").addClass("h200");
 		}
 	});
-	
-	$("body").on("blur", ".form_html textarea", function() {
+
+	$("body").on("blur", ".form_html textarea", function () {
 		$(this).removeClass("h200");
 	});
-	
+
 	/** 취업우대, 병역 클릭 처리 */
-	$(".benefit input[type='checkbox']").click(function() {
+	$(".benefit input[type='checkbox']").click(function () {
 		updateBenefit();
 	});
-	
+
 	/** 스크롤시 오른쪽 floating 메뉴 고정 */
 	updateNavFixed();
-	$(window).scroll(function() {
+	$(window).scroll(function () {
 		updateNavFixed();
 	});
-	
+
 	/** floating 메뉴 선택 처리 */
-	$(".floating_box input[type='checkbox']").click(function() {
+	$(".floating_box input[type='checkbox']").click(function () {
 		updateSelectedMenu();
 	});
-	
+
 	/** 이력서 저장하기 처리 */
-	$(".floating_box .save").click(function() {
+	$(".floating_box .save").click(function () {
 		if (confirm('정말 저장하시겠습니까?')) {
 			frmProfile.submit();
 		}
